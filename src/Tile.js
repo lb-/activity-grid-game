dba37c7d5d0329b5ce35591f3f7d1cc2ef141fa3
LB <mail@lb.ee>
2019-11-02 17:50:55 +1000

    make tiles rotate on click

--- src/Tile.js
+++ src/Tile.js
@@ -1,14 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Tile.css';
 
-const Tile = ({ children }) => (
-  <button className="Tile">
-    <i className="connector x" />
-    <i className="connector y" />
-    <i className="connector i" />
-    <i className="connector j" />
-    {children}
-  </button>
-);
+const Tile = ({ children }) => {
+  const [rotation, rotate] = useState(0);
+
+  return (
+    <button
+      className="Tile"
+      onClick={() => {
+        switch (rotation) {
+          case 0:
+            rotate(90);
+            break;
+          case 90:
+            rotate(180);
+            break;
+          case 180:
+            rotate(270);
+            break;
+          case 270:
+            rotate(0);
+            break;
+          default:
+            break;
+        }
+      }}
+      style={{ transform: `rotate(${rotation}deg)` }}
+    >
+      <span className="label">{children}</span>
+
+      <i className="connector x" />
+      <i className="connector y" />
+      <i className="connector i" />
+      <i className="connector j" />
+    </button>
+  );
+};
 
 export default Tile;
