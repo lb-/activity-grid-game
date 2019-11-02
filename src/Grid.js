f6dad6bdef529ce02d40ef98488d84c27150f486
LB <mail@lb.ee>
2019-11-02 17:54:28 +1000

    add sports data for the tiles

--- src/Grid.js
+++ src/Grid.js
@@ -1,15 +1,25 @@
 import React from 'react';
+import SPORTS from './SPORTS';
 import Tile from './Tile';
 import './Grid.css';
 
-const layout = [['a', 'b', 'c'], ['d', 'e', 'f'], ['g', 'h', 'i']];
+const sportsArray = Object.entries(SPORTS).map(([key, data]) => ({
+  key,
+  ...data
+}));
+
+const layout = [
+  [sportsArray[0], sportsArray[1], sportsArray[2]],
+  [sportsArray[3], sportsArray[4], sportsArray[5]],
+  [sportsArray[6], sportsArray[7], sportsArray[8]]
+];
 
 const Grid = () => (
   <div className="Grid">
     <table>
       {layout.map((tiles, index) => (
         <tr key={index}>
-          {tiles.map(key => (
+          {tiles.map(({ key }) => (
             <td key={key}>
               <Tile>{key}</Tile>
             </td>
---
+++ src/SPORTS.js
@@ -0,0 +1,42 @@
+// SOURCE: https://www.olympic.org/sports
+
+const SPORTS = {
+  archery: {
+    image: 'https://stillimg.olympic.org/sports/240x240/ar.png',
+    url: 'https://stillimg.olympic.org/sports/240x240/ar.png'
+  },
+  athletics: {
+    image: 'https://stillimg.olympic.org/sports/240x240/at.png',
+    url: 'https://www.olympic.org/athletics'
+  },
+  basketball: {
+    image: 'https://stillimg.olympic.org/sports/240x240/bk.png',
+    url: 'https://www.olympic.org/basketball'
+  },
+  diving: {
+    image: 'https://stillimg.olympic.org/sports/240x240/dv.png',
+    url: 'https://www.olympic.org/diving'
+  },
+  football: {
+    image: 'https://stillimg.olympic.org/sports/240x240/fb.png',
+    url: 'https://www.olympic.org/football'
+  },
+  hockey: {
+    image: 'https://stillimg.olympic.org/sports/240x240/ho.png',
+    url: 'https://www.olympic.org/hockey'
+  },
+  swimming: {
+    image: 'https://stillimg.olympic.org/sports/240x240/sw.png',
+    url: 'https://www.olympic.org/swimming'
+  },
+  triathlon: {
+    image: 'https://stillimg.olympic.org/sports/240x240/tr.png',
+    url: 'https://www.olympic.org/triathlon'
+  },
+  'water-polo': {
+    image: 'https://stillimg.olympic.org/sports/240x240/wp.png',
+    url: 'https://www.olympic.org/water-polo'
+  }
+};
+
+export default SPORTS;
