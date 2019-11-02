ef1455375d75394f55c179c2ffd219948f93e7cc
LB <mail@lb.ee>
2019-11-02 20:18:02 +1000

    Updates

---
+++ precache-manifest.2eeb545d27223ff44513ec94ccaacdc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cedd303e0b8b4ea1f2640de3cb3d5f47",
+    "url": "/activity-grid-game/index.html"
+  },
+  {
+    "revision": "db994d84bc1e2add2b3d",
+    "url": "/activity-grid-game/static/css/main.66790a3a.chunk.css"
+  },
+  {
+    "revision": "88357e6ec4a805296bf3",
+    "url": "/activity-grid-game/static/js/2.d4e43930.chunk.js"
+  },
+  {
+    "revision": "db994d84bc1e2add2b3d",
+    "url": "/activity-grid-game/static/js/main.f8a92bef.chunk.js"
+  },
+  {
+    "revision": "9330d11921bcf86c1ea9",
+    "url": "/activity-grid-game/static/js/runtime-main.45351bd8.js"
+  }
+]);
